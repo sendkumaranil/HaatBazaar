e036b1dcc5b2c07155e112ca587a7d1669b96827
Anil Kumar <sendkumaranil@gmail.com>
2018-02-18 20:28:27 +0530

    created new branch

--- src/main/java/com/hatbazar/onlineshopping/service/UserRegistrationService.java
+++ src/main/java/com/hatbazar/onlineshopping/service/UserRegistrationService.java
@@ -2,8 +2,13 @@ package com.hatbazar.onlineshopping.service;
 
 public class UserRegistrationService {
 	
-	public void addNewUser() {
-		
+	public void addNewUser(String emailid) {
+		boolean isUserPresent=isUserPresentinDB(emailid);
 	}
 
+	private boolean isUserPresentinDB(String emailid) {
+		// check user present in db or not here
+		return false;
+	}
+	
 }
